e6b96eba7ef843a70eb18a8cffa10fdf5a84f06b
William Thompson <wmthmps1@gmail.com>
2023-12-09 12:41:17 -0600

    Initial Commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -2,9 +2,38 @@
 {
     public class Program
     {
+        public static void FavSubjectStatement(string subject)
+        {
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine($"You're favorite subject is {subject}. You would probably enjoy Data Science or Software Development.");
+                    break;
+                case "Science":
+                    Console.WriteLine($"You're favorite subject is {subject}. You would probably enjoy Data Science, Software Development, or Medical Writing.");
+                    break;
+                case "English":
+                    Console.WriteLine($"You're favorite subject is {subject}. You would probably enjoy Copywriting.");
+                    break;
+                case "History":
+                    Console.WriteLine($"You're favorite subject is {subject}. You would probably enjoy Technical Writing.");
+                    break;
+                case "Art":
+                    Console.WriteLine($"You're favorite subject is {subject}. You would probably enjoy Web Design or Graphic Design.");
+                    break;
+                default:
+                    Console.WriteLine($"You're favorite subject is {subject}. I can't recommend a good remote job.");
+                    break;
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's see if I can recommend a good remote job for you.");
+            Console.WriteLine("What is your favorite school subject(make sure first letter is uppercase)?");
+            var favSubject = Console.ReadLine();
+            FavSubjectStatement(favSubject);
         }
     }
 }
